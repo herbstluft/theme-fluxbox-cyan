3a3d75ef180f4d791ecf9b98a1e49cee730ef737
herbstluft <herbstluftwm.28@gmail.com>
2021-10-26 12:22:44 -0500

    herbstluft

---
+++ instalar_tema_gtk2.sh
@@ -0,0 +1,7 @@
+sudo cp -r cyan-gtk2 /usr/share/themes/
+
+echo 
+echo  --------------------------------
+echo     Tema Instalado Correctamente
+echo  --------------------------------
+echo
---
+++ subir.sh
@@ -0,0 +1,6 @@
+git add .
+echo Espera....
+sleep 1s
+git commit -m "herbstluft"
+sleep 1s
+git push -u origin
